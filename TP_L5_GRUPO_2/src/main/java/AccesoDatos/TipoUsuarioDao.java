ab7beccafba2ee410732a01e508f9a22de0db6ba
SebastianFont <66184845+SebastianFont@users.noreply.github.com>
2020-07-09 16:17:44 -0300

    Add files via upload

--- TP_L5_GRUPO_2/src/main/java/AccesoDatos/TipoUsuarioDao.java
+++ TP_L5_GRUPO_2/src/main/java/AccesoDatos/TipoUsuarioDao.java
@@ -8,15 +8,31 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import Dominio.Tipo_Usuario;
 
 public class TipoUsuarioDao {
-
+	ApplicationContext appContext = new ClassPathXmlApplicationContext("Resources/Beans.xml");
 	public Tipo_Usuario UserCliente() {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("Resources/Beans.xml");
+		((ConfigurableApplicationContext)(appContext)).refresh();
 		try {
 			ConfigHibernate ch = new ConfigHibernate();
 			Session session = ch.abrirConexion();
 			Tipo_Usuario tipo = (Tipo_Usuario) appContext.getBean("BTipo_Usuario");
 			if ((Tipo_Usuario) session.createQuery(" FROM Tipo_Usuario u WHERE u.idTipoUsuario = 2").uniqueResult()!=null) {
 				tipo =(Tipo_Usuario) session.createQuery(" FROM Tipo_Usuario u WHERE u.idTipoUsuario = 2 ").uniqueResult();
+			} 
+		    session.close();
+		    return tipo;
+		} finally {
+			((ConfigurableApplicationContext)(appContext)).close();
+		}
+	}
+
+	public Tipo_Usuario UserBanco() {
+		((ConfigurableApplicationContext)(appContext)).refresh();
+		try {
+			ConfigHibernate ch = new ConfigHibernate();
+			Session session = ch.abrirConexion();
+			Tipo_Usuario tipo = (Tipo_Usuario) appContext.getBean("BTipo_Usuario");
+			if ((Tipo_Usuario) session.createQuery(" FROM Tipo_Usuario u WHERE u.idTipoUsuario = 1").uniqueResult()!=null) {
+				tipo =(Tipo_Usuario) session.createQuery(" FROM Tipo_Usuario u WHERE u.idTipoUsuario = 1 ").uniqueResult();
 			} 
 		    session.close();
 		    return tipo;
